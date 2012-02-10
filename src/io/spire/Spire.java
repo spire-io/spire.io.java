f6b176d038662834c664101cc35671628d6dc4d9
Jorge Gonzalez <jgonzalez@borderstylo.com>
2012-02-10 13:33:06 -0800

    Spire comments

--- src/io/spire/Spire.java
+++ src/io/spire/Spire.java
@@ -21,6 +21,19 @@ import java.lang.reflect.InvocationTargetException;
 
 
 /**
+ * Spire provides the main interface for authentication, session management,
+ * account management and creation different Spire resources
+ *  
+ *  <p>i.e.</p>
+ *  <p>{@code Spire spire = new Spire();}</p>
+ *  <p>{@code spire.discover();			// do Spire API discovery}</p>
+ *  <p>{@code spire.start(account_key)	// starts a new session}</p>
+ *  
+ * <p>
+ * Implementation is not thread-safe.
+ * </p>
+ * 
+ * @version 1.0
  * @author Jorge Gonzalez
  *
  */
@@ -34,7 +47,9 @@ public class Spire {
 	private Billing billing;
 
 	/**
+	 * Spire Default constructor.
 	 * 
+	 * Initialize spire object with default URL and API version
 	 */
 	public Spire(){
 		this.spire_url = SPIRE_URL;
@@ -42,8 +57,11 @@ public class Spire {
 	}
 	
 	/**
+	 * Construct from an url
 	 * 
-	 * @param url
+	 * Initialize spire object with specified 'url' and default API version
+	 * 
+	 * @param Spire Api entry point Url
 	 */
 	public Spire(String url){
 		this.spire_url = url;
@@ -51,76 +69,185 @@ public class Spire {
 	}
 	
 	/**
+	 * Construct from an Api entry point url and Api version
 	 * 
-	 * @param url
-	 * @param version
+	 * @param Spire Api url entry point
+	 * @param Api version
 	 */
 	public Spire(String url, String version){
 		this.spire_url = url;
 		this.api = new Api(spire_url, version);
 	}
 	
+	/**
+	 * Return the current Spire Api object
+	 * 
+	 * @return {@link io.spire.api.Api}
+	 */
 	public Api getApi(){
 		return api;
 	}
 	
+	/**
+	 * Returns the current Spire session object
+	 * 
+	 * @return {@link io.spire.api.Session}
+	 */
 	public Session getSession(){
 		return session;
 	}
 	
+	/**
+	 * Spire Api entry point. This method runs Spire Api discover process, 
+	 * retrieving the Api description for all available resources
+	 * 
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public void discover() throws ResponseException, IOException{
 		api.discover();
 	}
 	
+	/**
+	 * Creates an authenticated Spire session using the Spire Account key
+	 * 
+	 * @param accountKey
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public void start(String accountKey) throws ResponseException, IOException{
 		session  = api.createSession(accountKey);
 	}
 	
+	/**
+	 * Creates an authenticated Spire session using the account login and password.
+	 * 
+	 * @param email
+	 * @param password
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public void login(String email, String password) throws ResponseException, IOException{
 		session  = api.login(email, password);
 	}
 	
+	/**
+	 * Creates a new Spire account.
+	 * 
+	 * @param email
+	 * @param password
+	 * @param passwordConfirmation
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public void register(String email, String password, String passwordConfirmation) throws ResponseException, IOException{
 		session = api.createAccount(email, password, passwordConfirmation);
 	}
 	
-	public void passwordReset(String email) throws ResponseException, IOException{
-		
-	}
+//	public void passwordReset(String email) throws ResponseException, IOException{
+//		
+//	}
 	
+	/**
+	 * Deletes the current authenticated Spire account 
+	 * 
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public void deleteAccount() throws ResponseException, IOException{
 		session.getAccount().delete();
 	}
 	
-	public Channels getChannels() throws ResponseException, IOException{
+	/**
+	 * Gets all the Spire channels available in the current session
+	 * 
+	 * @return {@link Channels}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
+	public Channels getChannels(){
 		return session.getChannels();
 	}
 	
+	/**
+	 * Request and returns Spire Billing object with contains information about all
+	 * the billing plans available
+	 * 
+	 * @return {@link Billing}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public Billing billing() throws ResponseException, IOException{
 		billing = api.billing();
 		return billing;
 	}
 	
+	/**
+	 * Request and returns all Spire Channels available for the current session.
+	 * 
+	 * @return {@link Channels}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public Channels channels() throws ResponseException, IOException{
 		return session.channels();
 	}
 	
+	/**
+	 * Request the creation of a new Spire Channel
+	 * 
+	 * @param name of the channel to create
+	 * @return {@link Channel}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public Channel createChannel(String name) throws ResponseException, IOException{
 		return session.createChannel(name);
 	}
 	
+	/**
+	 * Gets all the Spire Subscriptions available in the current session
+	 * 
+	 * @return {@link Subscriptions}
+	 */
 	public Subscriptions getSubscriptions(){
 		return session.getSubscriptions();
 	}
 	
+	/**
+	 * Request and returns all Spire {@link Subscriptions} available for the current session.
+	 * 
+	 * @return {@link Subscriptions}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public Subscriptions subscriptions() throws ResponseException, IOException{
 		return session.subscriptions();
 	}
 	
+	/**
+	 * 
+	 * Creates a returns a new subscription.
+	 * 
+	 * @param name of the new {@link Subscription}
+	 * @param channels is a list of channel names to subscribe to
+	 * @return {@link Subscription}
+	 * @throws ResponseException
+	 * @throws IOException
+	 */
 	public Subscription subscribe(String name, String ...channels) throws ResponseException, IOException{
 		return session.subscribe(name, channels);
 	}
 	
+	/**
+	 * Spire Factory
+	 * Convenient for quick creation of Spire objects. 
+	 * Spire Factory reuses the internal Api description object 
+	 * obtained from the first originated {@link Api#discover()}
+	 * 
+	 * @author Jorge Gonzalez
+	 *
+	 */
 	public static class SpireFactory{
 		private static APIDescriptionModel description;
 		
@@ -131,21 +258,64 @@ public class Spire {
 			}
 		}
 		
+		/**
+		 * Creates a new instance of a Spire Channel
+		 * 
+		 * @return {@link Channel}
+		 * @throws ResponseException
+		 * @throws IOException
+		 */
 		public static Channel createChannel() throws ResponseException, IOException{
 			initialize();
 			return new Channel(description.schema);
 		}
 		
+		/**
+		 * Creates a new instance of a Spire Channel
+		 * The channel would be completely agnostic about the current
+		 * authenticated session. However, providing a capability and a Channel Uri
+		 * this channel could be used to publish messages
+		 * 
+		 * 
+		 * @param capability
+		 * @param url
+		 * @return {@link Channel}
+		 * @throws ResponseException
+		 * @throws IOException
+		 */
 		public static Channel createChannel(String capability, String url) throws ResponseException, IOException{
 			initialize();
 			return createResource(capability, url, Channel.class);
 		}
 		
+		/**
+		 * Creates a new instance of a Spire Subscription
+		 * The subscription would be completely agnostic about the current
+		 * authenticated session (if any). However, providing a capability and a 
+		 * Subscription Uri this subscription is capable of listening and retrieving messages
+		 * from the channels it is subscribed to.
+		 * 
+		 * @param capability
+		 * @param url
+		 * @return {@link Subscription}
+		 * @throws ResponseException
+		 * @throws IOException
+		 */
 		public static Subscription createSubscription(String capability, String url) throws ResponseException, IOException{
 			initialize();
 			return createResource(capability, url, Subscription.class);
 		}
 		
+		/**
+		 * Creates new instances of spire resources
+		 * 
+		 * @param capability
+		 * @param url
+		 * @param T is the class of the Spire Resource to create
+		 * @return T new instance
+		 * @throws ResponseException
+		 * @throws IOException
+		 */
 		public static <T>T createResource(String capability, String url, Class<T> T) throws ResponseException, IOException{
 			initialize();
 			Constructor<T> constructorT = null;
